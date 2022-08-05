e11c2e0ba1751d6ad225476fbca4859c97a4d483
hamed.refaat <hamed.elhassanien@talabat.com>
2022-08-05 03:06:48 +0200

    Remove Static Methods

---
+++ FilterAndRank/FilterProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FilterAndRank
+{
+  public  class FilterProcessor
+    {
+        public FilterProcessor()
+        {
+
+        }
+
+        public  IEnumerable<long> PeopelWithoutRanking(IList<Person> people, IList<CountryRanking> rankingData)
+        {
+            var peopleIdes = people.Select(i => i.Id);
+            var peopleIdesInRank = rankingData.Select(i => i.PersonId);
+            var peopleIdsWithoutRanking = peopleIdes.Except(peopleIdesInRank);
+            return peopleIdsWithoutRanking;
+        }
+        public IEnumerable<long> PeopleNotInCountryFilter(IList<CountryRanking> rankingData, IList<string> countryFilter)
+        {
+            return rankingData
+                            .Where(rd => FilterPeopelByCountryFilter(countryFilter, rd))
+                            .Select(rd => rd.PersonId);
+        }
+        public IEnumerable<long> OutOfRankPeople(IList<CountryRanking> rankingData, int minRank, int maxRank)
+        {
+            return rankingData
+                            .Where(rank => FilterPeopelInRank(minRank, maxRank, rank))
+                            .Select(i => i.PersonId);
+        }
+
+
+        private  bool FilterPeopelByCountryFilter(IList<string> countryFilter, CountryRanking countryRanking)
+        {
+            return !countryFilter.ToLower().Contains(countryRanking.Country.ToLower());
+        }
+        private  bool FilterPeopelInRank(int minRank, int maxRank, CountryRanking rank)
+        {
+            return !(rank.Rank >= minRank && rank.Rank <= maxRank);
+        }
+    }
+}
--- FilterAndRank/Ranking.cs
+++ FilterAndRank/Ranking.cs
@@ -15,10 +15,11 @@ namespace FilterAndRank
         {
             // TODO: write your solution here.  Do not change the method signature in any way, or validation of your solution will fail.
             IList<CountryRanking> PeopelRankingWithoutLimit = null;
+            FilterProcessor filterProcessor = new();
             return rankingData
-                .Filter(PeopelWithoutRanking(people, rankingData))
-                .Filter(PeopleNotInCountryFilter(rankingData, countryFilter))
-                .Filter(OutOfRankPeople(rankingData, minRank, maxRank))
+                .Filter(filterProcessor.PeopelWithoutRanking(people, rankingData))
+                .Filter(filterProcessor.PeopleNotInCountryFilter(rankingData, countryFilter))
+                .Filter(filterProcessor.OutOfRankPeople(rankingData, minRank, maxRank))
                 .ApplyCustomOrder(people, countryFilter)
                 .CloneTo(ref PeopelRankingWithoutLimit)
                 .Limit(maxCount)
@@ -27,34 +28,6 @@ namespace FilterAndRank
                 .ToList();
         }
 
-        private static IEnumerable<long> PeopelWithoutRanking(IList<Person> people, IList<CountryRanking> rankingData)
-        {
-            var peopleIdes = people.Select(i => i.Id);
-            var peopleIdesInRank = rankingData.Select(i => i.PersonId);
-            var peopleIdsWithoutRanking = peopleIdes.Except(peopleIdesInRank);
-            return peopleIdsWithoutRanking;
-        }
-        private static IEnumerable<long> PeopleNotInCountryFilter(IList<CountryRanking> rankingData, IList<string> countryFilter)
-        {
-            return rankingData
-                            .Where(rd => FilterPeopelByCountryFilter(countryFilter, rd))
-                            .Select(rd => rd.PersonId);
-        }
-        private static IEnumerable<long> OutOfRankPeople(IList<CountryRanking> rankingData, int minRank, int maxRank)
-        {
-            return rankingData
-                            .Where(rank => FilterPeopelInRank(minRank, maxRank, rank))
-                            .Select(i => i.PersonId);
-        }
-
-      
-        private static bool FilterPeopelByCountryFilter(IList<string> countryFilter, CountryRanking countryRanking)
-        {
-            return !countryFilter.ToLower().Contains(countryRanking.Country.ToLower());
-        }
-        private static bool FilterPeopelInRank(int minRank, int maxRank, CountryRanking rank)
-        {
-            return !(rank.Rank >= minRank && rank.Rank <= maxRank);
-        }
+       
     }
 }
